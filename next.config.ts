40223f69eb40869e872b021b792e65eed7613b56
MrPersonManGuy <86075768+MrPersonManGuy@users.noreply.github.com>
2026-05-15 19:18:32 -0500

    yeah

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next'
 import { withContentlayer } from 'next-contentlayer2'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
